d5044ca283ef7385c9f1fc43a675eac3e70123b6
WilliamPrakash <willprakash@gmail.com>
2021-07-19 22:30:35 -0400

    Removed newline char error and can now compare characters

--- Lexical_Analyzer/lexical_analyzer.c
+++ Lexical_Analyzer/lexical_analyzer.c
@@ -11,39 +11,51 @@ int main(int argc, char *argv[]) {
 	size_t bufsize = 32;
 	buffer = (char *)malloc(bufsize * sizeof(char));
 	getline(&buffer, &bufsize, stdin);
-	buffer[strcspn(buffer, "\n")];	// gets rid of newline character
+	// gets rid of newline character
+	void removeNewLineCharacter(char *ptr){
+		while( (ptr != NULL) && (*ptr != '\n') ){
+			++ptr;
+		}
+		*ptr = '\0';
+	}
+	removeNewLineCharacter(buffer);
 
-	// Tokenize buffer by char to read it as a char array (string)
+	// Initialize array to hold tokenized buffer
 	const char s[2] = " ";	// tokenize buffer based on a blank space
 	char *token;	// this probably isn't memory safe, stack overflow vulnerability
 	token = strtok(buffer, s);
 	int i = 0;
 	char *arr[20];
 	arr[i] = token;
+	int arrCount = 0;
 
+	// Tokenize buffer after assigning the first element to arr[0]
 	while(token != NULL){
 		token = strtok(NULL, s);
 		if(token == NULL){
 			break;	// gets rid of segmentation fault error
 		}
-		//printf("%s", token);
+		arrCount++;
 		i++;
 		arr[i] = token;
 	}
 
-	i = 0;
-	// print out contents
+	// counts for different types of tokens???
+	
+	// IDEA: add brackets to a stack data structure to ensure equal opening/closing brackets
+
+	//i = 0;
+	// Parse through array
 	while(i < 20){
-		printf("%s ", arr[i]);
+		if(arr[i] == NULL){
+			break;
+		}
+		if( (strcmp(arr[i], "(") == 0)) { 
+			printf("Memory address of %s: %p\n", arr[i], &arr[i]);
+			printf("It works\n");
+		}
 		i++;
 	}
-
-	// get and store user input in char array
-	/*char str[100];
-	printf("Enter a value :");
-	scanf("%s", str);
-	getchar();
-	printf("\nYou entered: %s\n", str);*/
 
 	return 0;
 }
