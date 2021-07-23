d81e7923b5ee23d79ab9dbaa004eca7b6be5220e
WilliamPrakash <willprakash@gmail.com>
2021-07-22 23:02:28 -0400

    Cleaning up

--- intro-c/pointer-1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-	int i = 123;
-	printf("Variable i has addr (%p) and value %d\n", &i, i);
-	return 0;
-}
--- intro-c/pointer-2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-	int i = 123;
-	int *p_i = &i;
-	printf("Variable i has addr (%p) and value %d\n", &i, i);
-	printf("The pointer points to addr (%p) containing value %d\n", p_i, *p_i);
-	return 0;
-}
