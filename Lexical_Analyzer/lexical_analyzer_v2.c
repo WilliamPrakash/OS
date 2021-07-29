1949615e0baa1a25bfa4edfb5f093d7d21d09ede
WilliamPrakash <willprakash@gmail.com>
2021-07-28 20:11:00 -0400

    can now parse through multiple ints to recognize an int larger than one number

--- Lexical_Analyzer/lexical_analyzer_v2.c
+++ Lexical_Analyzer/lexical_analyzer_v2.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
 	// memory safe way of getting input (puts it on heap, clear heap memory?)
@@ -12,61 +13,90 @@ int main(int argc, char *argv[]) {
 	buffer = (char *)malloc(bufsize * sizeof(char));
 	getline(&buffer, &bufsize, stdin);
 	int i = 0;
+	int bufCt = 0;
 	// iterate through buffer
+	printf("Contents of buffer: ");
 	for (i = 0; buffer[i] != '\0'; i++){
 		printf(" %c  ", buffer[i]);
+		bufCt++;
 	}
-	
-	// iterate through buffer checking input
-	
+	printf("\n");
 
+	// arrays for holding ints and letters
+	char *temp;
+	size_t tempSize = 32;
+	temp = (char *)malloc(tempSize * sizeof(char));
+	int *numbers;
+	size_t numbersSize = 32;
+	numbers = (int *)malloc(numbersSize * sizeof(int));
+	//char *strings;
+	//size_t stringsSize = 32;
+	//strings = (char *)malloc(stringsSize * sizeof(char));
+	int k = 0;
+	//int l = 0;
+	int num;
+	//char str;
+	// iterate through buffer, parsing ints, strings, brackets
+	for(i = 0; i < bufCt; i++){
+		if(isdigit(buffer[i])) {
+			int j = 0;
+			while(isdigit(buffer[i])) {
+				temp[j] = buffer[i];
+				i++;
+				j++;
+			}
+			// convert temp to an int and add it to numbers array
+			sscanf(temp, "%d", &num);
+			numbers[k] = num;
+			// clear temp 
+			memset(temp, 0, sizeof temp);
+			k++;
+		}
+		/*if(ischar(buffer[i])) {
+			int j = 0;
+			while(ischar(buffer[i])) {
+				temp[j] = buffer[i];
+				i++;
+				j++;
+			}
+			sscanf(temp, "%c", &str);
+			strings[l] = str;
+			memset(temp, 0, sizeof temp);
+			l++;
+		}*/
+	}
 
+	/*printf("Contents of temp ");
+	for(i = 0; temp[i] != '\0'; i++){
+		printf(" %c  ", temp[i]);
+	}*/
+	printf("\n");;
+	for(i = 0; i < k; i++ ){
+		printf("Num in members: %d", numbers[i]);
+	}
 
-	
-	// TODO: need to find a way to get all the characters in buffer, cause this shit isn't working
-	/*int i = 0;
-	char *token;
-	const char s[2] = "";	// I don't this is working;
-	token = strtok(buffer, s);
-	char *arr[20];
-	arr[i] = token;
-	while(token != NULL ) {
-		token = strtok(NULL, s);
-		if(token == NULL) { break;  };
-		i++;
-		arr[i] = token;
+	/*printf("\n");
+	for(i = 0; i < l; i++) {
+		printf("String in strings %c", strings[i]);
 	}*/
 
-	// this is only printing . a, I think there is an issue with the tokenizing while loop above
-	/*i = 0;
-	printf("All characters in arr: \n");
-	while(arr[i] != NULL){
-		printf(". %c ", *arr[i]);
-		i++;
-	}
-	printf("\n");*/
+
+
+
+
+
+
+
+	// iterate through buffer checking input
 
 	// need a way to tokenize not just based on space, that wouldn't account for this: if(statement){    -> it would get treated as one
 	// maybe add every single individual char to array, then tokenize
 	// it would iterate over each array element, if it was a number, it would keep going until it hit a non number
-	// it would add the number ^^^ as it's own whole string to an array
-
-	/*
-	i = 0;
-	// Tokenize buffer after assigning the first element to arr[0]
-	while(token != NULL){
-		token = strtok(NULL, s);
-		if(token == NULL){
-			break;	// gets rid of segmentation fault error
-		}
-		arrCount++;
-		i++;
-		arr[i] = token;
-	}
+	// it would add the number ^^^ as it's own whole string to 
 
 	// IDEA: initialize char arrays for all different types of tokens
 	// I'll need a way to tell the order of tokens going forward to make a compiler
-	char leftBrackets[10];
+	/*char leftBrackets[10];
 	int leftBracCount = 0;
 	char rightBrackets[10];
 	int rightBracCount = 0;
@@ -89,27 +119,7 @@ int main(int argc, char *argv[]) {
 			opCt++;
 		} else{}
 		i++;
-	}
-
-	// print contents of right and left bracket char arrays
-	if(leftBracCount != rightBracCount){
-		printf("Inequal amount of left and right brackets\n");
-	} else {
-		i = 0;
-		while(i < leftBracCount){
-			printf("LB:  %c\n", leftBrackets[i]);
-			printf("RB: %c\n", rightBrackets[i]);
-			i++;
-		}
-	}
-	i = 0;
-	printf("Operators passed: ");
-	while(i < opCt){
-		printf("%c ",operators[i] );
-		i++;
-	}
-
-	*/
+	}*/
 
 	printf("\n");
 	return 0;
