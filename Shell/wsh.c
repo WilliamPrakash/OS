e1b93961e65651b90a8b03d04a1e90b32e5c8590
WilliamPrakash <willprakash@gmail.com>
2021-08-03 22:10:09 -0400

    added basic process creation to wsh

--- Shell/wsh.c
+++ Shell/wsh.c
@@ -1,44 +1,62 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <pthread.h>
+//#include <pthread.h>	// not even using this... yet
 #include <string.h>
-// remember to compile this with the pthread flag
+#include <unistd.h>
+#include <sys/wait.h>
+//  NOTE: compile this with -lpthread flag
+// fork() lets you create and run a copy of the original process
+// exec() lets you run a different process in place of the copy of the original process
 
 int main(int argv, char *argc[]) {
 	char *buffer;
 	size_t bufsize = 32;
 	buffer = (char *)malloc(bufsize * sizeof(char));
-	printf("-> ");
-	int i;
-	getline(&buffer, &bufsize, stdin);
-	
-	void removeNewLineCharacter(char *ptr){
-		while( (ptr != NULL) && (*ptr != '\n') ){
-			++ptr;
+
+	while(strcmp(buffer, "exit") != 0) {		
+		memset(buffer, 0, sizeof buffer);	// clear buffer before a new command is taken in
+		printf("-> ");
+        	int i;
+	        getline(&buffer, &bufsize, stdin);
+        
+        	void removeNewLineCharacter(char *ptr){		// removes newline from buffer
+                	while( (ptr != NULL) && (*ptr != '\n') ){
+                        	++ptr;
+	                }
+        	        *ptr = '\0';
+	        }
+	        removeNewLineCharacter(buffer);
+
+        	i = 0;
+       		 /*while(buffer[i] != '\n') {
+                	printf(" %c ", buffer[i]);
+	                i++;
+        	}*/
+
+	        if(strcmp(buffer, "ls") == 0) {			
+	                printf("ls command recognized\n");
 		}
-		*ptr = '\0';
-	}
-	removeNewLineCharacter(buffer);
+		
+		if(strcmp(buffer, "create process") == 0) {
+			//printf("Main process pid: %d\n", (int) getpid());
+			int child = fork();	// returns an id of 0 to the child process -> different from pid?
+			if(child < 0) {
+				fprintf(stderr, "fork failed\n");
+				exit(1);
+			} else if (child == 0) {
+				printf("hello from child process number %d\n", (int) getpid());
+				//sleep(1);
+				//kill(child, SIGTERM);
+				exit(0);
+			} else {
+				//int wc = wait(NULL);
+				wait(NULL);
+				//kill(child, SIGTERM);
+				printf("hello from parent process %d\n", (int) getpid());
+			}
+		}
 
-	printf("Contents of buffer: ");
-	for (i = 0; buffer[i] != '\0'; i++) {
-		printf(" %c  ", buffer[i]);
 	}
-	printf("\n");
-	//char *input[10];
-	//sscanf(input, "%s", buffer);
-
-	i = 0;
-	/*while(buffer[i] != '\n') {
-		printf(" %c ", buffer[i]);
-		i++;
-	}*/
-	
-	// while loop for interactivity
-	if(strcmp(buffer, "ls") == 0) {
-		printf("ls command recognized\n");
-	}
-
 
 	return 0;	
 }
