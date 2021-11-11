0fcee77e34d36d433f54e2071a9985875a8d36f5
WilliamPrakash <willprakash@gmail.com>
2021-11-10 21:44:43 -0500

    Semi working thread id/unique values

--- CPU_Virtualization/cpu_scheduler_v2.c
+++ CPU_Virtualization/cpu_scheduler_v2.c
@@ -10,6 +10,7 @@ void *roll();
 void *timer();
 void *tls_values();
 void *do_stuff();
+void *initialize_thread();
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cv;
@@ -19,13 +20,10 @@ pthread_key_t glob_var_key;
 
 int main(int argc, char *argv[]) {
 	pthread_t th[2];
-	pthread_key_create_(&global_var_key, NULL);
-
-	int *res = malloc(sizeof(int));
-	srand(time(NULL));
+	pthread_key_create(&glob_var_key, NULL);
 
 	for(int i = 0; i < 2; i++) {
-		if(pthread_create(th + i, NULL, tls_values, NULL) != 0) {
+		if(pthread_create(th + i, NULL, initialize_thread, NULL) != 0) {
 			perror("Failed to create thread\n");
 			return 1;
 		}
@@ -33,42 +31,30 @@ int main(int argc, char *argv[]) {
 	}
 	for(int i = 0; i < 2; i++) {
 		if(pthread_join(th[i], NULL) != 0) {
-		//if(pthread_join(th[i], (void**) &res) != 0) {
 			perror("Failed to join thread\n");
                         return 1;
                 }
-		//printf("Result: %d\n", *res);
-		//free(res);
 	}
 	pthread_mutex_destroy(&mutex);
 	exit(0);
 }
 
-void *do_stuff() {
-	// get thread specific data
-        int* glob_spec_var = pthread_getspecific(glob_var_key);
-        printf("Thread %d before mod value is %d\n", (unsigned int) pthread_self(), *glob_spec_var);
-        *glob_spec_var += 1;
-        printf("Thread %d after mod value is %d\n", (unsigned int) pthread_self(), *glob_spec_var);
-}
-
-void *tls_values() {
-	int *p = malloc(sizeof(int));
-	*p = 1;
-	printf("Address of p for thread %d: %p\n", (unsigned int) pthread_self(), p);
-        pthread_setspecific(glob_var_key, p); // i think the only way this works is if glob_var_key is unique to each thread 
-	do_stuff();
-	do_stuff();
-	pthread_setspecific(glob_var_key, NULL); // why is this called? oh I think I'm clearing out the value associated with glob_var_key since I'm creating this var on the heap
-        free(p);
-        pthread_exit(NULL);
+void *initialize_thread() {
+	int *tid = malloc(sizeof(int));
+	*tid = rand() % 10; // this doesn't guarantee duplicates
+	pthread_setspecific(glob_var_key, tid);
+	mutex_test();
+	pthread_setspecific(glob_var_key, NULL);
+	free(tid);
+	pthread_exit(NULL);
 }
 
 void *mutex_test() {
+	int* tid = pthread_getspecific(glob_var_key);
+	printf("unique thread value: %d\n", *tid);
 	pthread_cond_init(&cv, NULL);
-	// TODO: Pause thread execution and giving it to another
-	// i need a variable that's specific to each thread if I wanna make a loop
 	//while(1) {
+	//while(*tid != *tid + 1) {
 		if(pthread_mutex_trylock(&mutex) == 0) {
 			pthread_mutex_lock(&mutex);
 			printf("mutex has been locked\nPerforming a task for 3 seconds...\n");
@@ -76,6 +62,7 @@ void *mutex_test() {
 			pthread_mutex_unlock(&mutex);
 			printf("unlocked\n");
 			pthread_cond_signal(&cv);
+			//*tid += 1;
         	} else {
 			printf("pausing other thread while waiting for mutex to be unlocked\n");
 			pthread_cond_wait(&cv, &mutex); // this will wait for a signal from another thead
@@ -85,3 +72,25 @@ void *mutex_test() {
 	pthread_cond_destroy(&cv);
 }
 
+
+
+/****	Reference code for TLS	****/
+void *do_stuff() {
+        // get thread specific data
+        int* glob_spec_var = pthread_getspecific(glob_var_key);
+        printf("Thread %d before mod value is %d\n", (unsigned int) pthread_self(), *glob_spec_var);
+        *glob_spec_var += 1;
+        printf("Thread %d after mod value is %d\n", (unsigned int) pthread_self(), *glob_spec_var);
+}
+
+void *tls_values() {
+        int *p = malloc(sizeof(int));
+        *p = rand() % 10;
+        printf("Address of p for thread %d: %p\n", (unsigned int) pthread_self(), p);
+        pthread_setspecific(glob_var_key, p); // i think the only way this works is if glob_var_key is unique to each thread
+        do_stuff();
+        do_stuff();
+        pthread_setspecific(glob_var_key, NULL); // why is this called? oh I think I'm clearing out the value associated with glob_var_key since I'm creating this var on the heap
+        free(p);
+        pthread_exit(NULL);
+}
