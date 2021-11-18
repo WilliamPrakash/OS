a7770f1363a62e183a1913a98d97bf0e9e0828dc
WilliamPrakash <willprakash@gmail.com>
2021-11-17 20:18:48 -0500

    Close to alternating threads, running into a segmentation fault error

--- CPU_Virtualization/cpu_scheduler_v2.c
+++ CPU_Virtualization/cpu_scheduler_v2.c
@@ -6,12 +6,7 @@
 #include <pthread.h>
 
 void *mutex_test();
-void *roll();
-void *timer();
-void *tls_values();
-void *do_stuff();
 void *initialize_thread();
-void *force_thread_sleep();
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cv;
@@ -20,21 +15,19 @@ pthread_key_t glob_var_key_2;	// duplicate for comparison
 
 int thread_ct = 2;	// global count for thread-specific conditional checks
 unsigned long int tid_list[2] = {0,0};
+int *indx;
 
 int main(int argc, char *argv[]) {
 	pthread_t th[thread_ct];
 	pthread_key_create(&glob_var_key, NULL);
 	pthread_key_create(&glob_var_key_2, NULL);
-	
+	indx = malloc(sizeof(int));
+	*indx = 0;
 	for(int i = 0; i < thread_ct; i++) {
 		if(pthread_create(th + i, NULL, initialize_thread, NULL) != 0) {
 			perror("Failed to create thread\n");
 			return 1;
 		}
-		/****	can't do this here, i think it's only adding main thread	****/
-		/*tid_list[i] = pthread_self();
-		printf("Thread %ld currently executing...\n", pthread_self());
-		printf("Thread %ld added to tid_list\n", tid_list[i]);*/
 	}
 	for(int i = 0; i < thread_ct; i++) {
 		if(pthread_join(th[i], NULL) != 0) {
@@ -48,23 +41,17 @@ int main(int argc, char *argv[]) {
 
 void *initialize_thread() {
 	int *tid = malloc(sizeof(int));
-
 	*tid = rand() % 10; // this doesn't guarantee duplicates
 	int* dup = malloc(sizeof(int));
-	*dup = *tid;
-	printf("tid for thread %ld: %d\n", pthread_self(), *tid);
-	//
+	*dup = *tid + 3;
 	int i = 0;
 	while(tid_list[i] != 0) {
 		i++;
 	}
 	tid_list[i] = pthread_self();
-	printf("thread %ld added to tid_list\n", tid_list[i]);
-	
+	// each thread needs two values, one to increment, and a duplicate to compare
 	pthread_setspecific(glob_var_key, tid);		// increment
 	pthread_setspecific(glob_var_key_2, dup);	// compare
-	//free(tid);
-	//free(dup);
 	mutex_test();
 	pthread_setspecific(glob_var_key, NULL);
 	pthread_setspecific(glob_var_key_2, NULL);
@@ -73,41 +60,33 @@ void *initialize_thread() {
 	pthread_exit(NULL);
 }
 
-int indx = 0;
 void *mutex_test() {
-	//int* tid = pthread_getspecific(glob_var_key);
-	int* dup = pthread_getspecific(glob_var_key_2);
-	*dup = *dup + 3;
-	pthread_setspecific(glob_var_key_2, dup);
 	pthread_cond_init(&cv, NULL);
 	
-	while(pthread_getspecific(glob_var_key) <= pthread_getspecific(glob_var_key_2) ) {
-		printf("value of indx before trylock: %d\n", indx);
-		if( (pthread_mutex_trylock(&mutex) == 0) && (pthread_self() == tid_list[indx]) ) {
-			printf("mutex has been locked, doing stuff\n");
-			/**  this will only work for two threads, need to use thread_ct  **/
-			printf("value of indx: %d\n", indx);
-			/*if(indx >= 1) {
-				printf("skjdgfa jysgdfgasjdfgs\n");
-				indx = 0;
-			} else {*/
-				indx = 1;
-			//}
-			sleep(3);
+	// the two global variables set in initialize_thread() control how many loops a thread does
+	// what if a thread is done? it should be removed from the tid_list!!!!!!
 
-			// increment
+	while(pthread_getspecific(glob_var_key) <= pthread_getspecific(glob_var_key_2) ) {
+		printf("value of indx before trylock: %d\n", *indx);
+		// maybe make two separate if checks, one for pthread_self and the next for mutex
+		if( (pthread_mutex_trylock(&mutex) == 0) && (pthread_self() == tid_list[*indx]) ) {
+			printf("mutex has been locked, doing stuff\n");
+			sleep(3);
+			if(*indx == 0) *indx = 1;
+			else if(*indx == 1) *indx = 0;
+			printf("value of indx after edits in mutex: %d\n", *indx);
+
+			// increment key
 			int* temp = pthread_getspecific(glob_var_key);
 			*temp = *temp + 1;
 			pthread_setspecific(glob_var_key, temp);
-			printf("glob_var_key for thread %ld: %d\n", pthread_self(), *temp);
-			printf("unlocked\n");
 			pthread_cond_signal(&cv);
 			pthread_mutex_unlock(&mutex);
 		} else {
 			pthread_cond_wait(&cv, &mutex);
 			indx = 0;
 		}
-		printf("1 loop completed: \n");
+		//printf("1 loop completed: \n");
 	}
 	printf("exiting while loop...\n");
 	pthread_cond_destroy(&cv);
@@ -115,25 +94,3 @@ void *mutex_test() {
 
 
 
-
-
-/****	Reference code for TLS	****/
-void *do_stuff() {
-        // get thread specific data
-        int* glob_spec_var = pthread_getspecific(glob_var_key);
-        printf("Thread %d before mod value is %d\n", (unsigned int) pthread_self(), *glob_spec_var);
-        *glob_spec_var += 1;
-        printf("Thread %d after mod value is %d\n", (unsigned int) pthread_self(), *glob_spec_var);
-}
-
-void *tls_values() {
-        int *p = malloc(sizeof(int));
-        *p = rand() % 10;
-        printf("Address of p for thread %d: %p\n", (unsigned int) pthread_self(), p);
-        pthread_setspecific(glob_var_key, p); // i think the only way this works is if glob_var_key is unique to each thread
-        do_stuff();
-        do_stuff();
-        pthread_setspecific(glob_var_key, NULL); // why is this called? oh I think I'm clearing out the value associated with glob_var_key since I'm creating this var on the heap
-        free(p);
-        pthread_exit(NULL);
-}
