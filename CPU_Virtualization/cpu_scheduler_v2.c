a6008e3a6a4b5021221902815d0711be4ee70a3a
WilliamPrakash <willprakash@gmail.com>
2021-11-23 20:20:47 -0500

    closer to figuring out why the round robin algorithm isn't switching properly

--- CPU_Virtualization/cpu_scheduler_v2.c
+++ CPU_Virtualization/cpu_scheduler_v2.c
@@ -5,8 +5,8 @@
 #include <string.h>
 #include <pthread.h>
 
-void *mutex_test();
-void *initialize_thread();
+void *round_robin();
+void* initialize_thread(void *input);
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cv;
@@ -16,15 +16,22 @@ pthread_key_t glob_var_key_2;	// duplicate for comparison
 int thread_ct = 2;	// global count for thread-specific conditional checks
 unsigned long int tid_list[2] = {0,0};
 int *indx;
+int isFirst = 0;
+struct thread_data {
+	int num;
+};
+
 
 int main(int argc, char *argv[]) {
 	pthread_t th[thread_ct];
 	pthread_key_create(&glob_var_key, NULL);
 	pthread_key_create(&glob_var_key_2, NULL);
 	indx = malloc(sizeof(int));
-	*indx = 0;
+	struct thread_data *info;
 	for(int i = 0; i < thread_ct; i++) {
-		if(pthread_create(th + i, NULL, initialize_thread, NULL) != 0) {
+		info = malloc(sizeof(struct thread_data));
+		info->num = i;
+		if(pthread_create(th + i, NULL, initialize_thread, info) != 0) {
 			perror("Failed to create thread\n");
 			return 1;
 		}
@@ -39,42 +46,50 @@ int main(int argc, char *argv[]) {
 	exit(0);
 }
 
-void *initialize_thread() {
-	int *tid = malloc(sizeof(int));
-	*tid = rand() % 10; // this doesn't guarantee duplicates
+void* initialize_thread(void *input) {
+	int num;
+	struct thread_data *info = input;
+	int *inc = malloc(sizeof(int));
+	*inc = rand() % 10;
 	int* dup = malloc(sizeof(int));
-	*dup = *tid + 3;
-	int i = 0;
-	while(tid_list[i] != 0) {
-		i++;
+	*dup = *inc + 3;
+	tid_list[info->num] = pthread_self();
+	if(isFirst == 0) {
+		*indx = info->num;
+		isFirst += 1;
 	}
-	tid_list[i] = pthread_self();
+
 	// each thread needs two values, one to increment, and a duplicate to compare
-	pthread_setspecific(glob_var_key, tid);		// increment
+	pthread_setspecific(glob_var_key, inc);		// increment
 	pthread_setspecific(glob_var_key_2, dup);	// compare
-	mutex_test();
+	// running into an issue of which thread created above actually calls this round_robin function
+	/* before I call this, I need to make sure both threads are added to the tid_list*/
+	/* I'm running into an error where one thread starts execution of round_robin before the other even has a chance to call the function*/
+	/* ^^^ this messes up the check of tid_list[*indx], as if a thread is already checking that and the other thread hasn't added to this global variable, then it's checking against nothing*/
+	round_robin();
 	pthread_setspecific(glob_var_key, NULL);
 	pthread_setspecific(glob_var_key_2, NULL);
-	free(tid);
+	free(inc);
 	free(dup);
 	pthread_exit(NULL);
 }
 
-void *mutex_test() {
+/* this thread sometimes works, sometimes doesn't. I think I'm not checking the order of when threads are added to the tid_list*/
+void *round_robin() {
 	pthread_cond_init(&cv, NULL);
-	
-	// the two global variables set in initialize_thread() control how many loops a thread does
-	// what if a thread is done? it should be removed from the tid_list!!!!!!
-	
-	// NEW ISSUE: runs and switches correctly, but stops at some point and doesn't exit while loop
+	// what if a thread is done? it should be removed from the tid_list
 
+	// this might not check values but rather memory address locations
 	while(pthread_getspecific(glob_var_key) <= pthread_getspecific(glob_var_key_2) ) {
 		printf("next tid to be run: %ld\n", tid_list[*indx]);
+		//printf("glob_var_key: %p, glob_var_key_2: %p\n", pthread_getspecific(glob_var_key), pthread_getspecific(glob_var_key_2));
 		if( (pthread_mutex_trylock(&mutex) == 0) && (pthread_self() == tid_list[*indx]) ) {
+			printf("thread in mutex: %ld\n", pthread_self());
 			printf("mutex has been locked, doing stuff\n");
 			sleep(3);
 			if(*indx == 0) *indx = 1;
 			else if(*indx == 1) *indx = 0;
+			printf("new value of *indx: %d\n", *indx);
 
 			// increment key
 			int* temp = pthread_getspecific(glob_var_key);
