08fa402a46fdfcaced3c2bd48426901970b02969
WilliamPrakash <willprakash@gmail.com>
2022-04-07 20:14:17 -0400

    added file to mess around with alongside fs.c tutorial

---
+++ File_System/fs_2.c
@@ -0,0 +1,62 @@
+#include "fs.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct superblock sb;
+struct inode *inodes;
+struct disk_block *dbs;
+
+void create_fs() {
+	// setup superblock
+	sb.num_inodes = 1;
+	sb.num_blocks = 2;
+	sb.size_blocks = sizeof(struct disk_block);
+	
+	int i;
+
+	// initialize nodes
+	inodes = malloc(sizeof(struct inode) * sb.num_inodes);
+	for(i = 0; i < sb.num_inodes; i++) {
+		inodes[i].size = -1;
+		// arguments: (destination, source)
+		strcpy(inodes[i].name, "");
+	}
+
+	// initialize disks
+	dbs = malloc(sizeof(struct disk_block) * sb.num_blocks);
+	for(i = 0; i < sb.num_blocks; i++) {
+		dbs[i].next_block_num = -1;
+		// no need to initialize the data here
+	}
+}
+
+void mount_fs() {
+
+}
+
+void sync_fs() {
+	FILE *file;
+	file = fopen("filesystem_mk1", "w+");
+	
+	// superblock
+	fwrite(&sb, sizeof(struct superblock), 1, file);
+
+	// inodes
+	int i;
+	for(i = 0; i < sb.num_inodes; i++) {
+        	fwrite(&inodes[i], sizeof(struct inode), 1, file);
+        }
+
+        // initialize disks
+        dbs = malloc(sizeof(struct disk_block) * sb.num_blocks);
+        for(i = 0; i < sb.num_blocks; i++) {
+		fwrite(&dbs[i], sizeof(struct disk_block), 1, file);
+        }
+	fclose(file);
+}
+
+
+
+
+
