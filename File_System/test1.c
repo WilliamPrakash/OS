764619c4549ec1d6a42d022c2e81573355ed1181
WilliamPrakash <willprakash@gmail.com>
2022-05-23 19:38:36 -0400

    Don't remember what I wrote, hope it was good

--- File_System/test1.c
+++ File_System/test1.c
@@ -2,10 +2,22 @@
 #include "fs.c"
 
 void main() {
-	create_fs();
-	sync_fs();
+	// create filesystem
+	//create_fs();
+	//sync_fs();
+
+	char str[20];
+	int i;
+	printf("Enter a string and an int:   ");
+	scanf("%s %d", str, &i);	
+	printf("\nYou entered: %s %d ", str, i);
+	// always need to mount it before performing any action on it
+	// except maybe printing it?
 	mount_fs();
 	allocate_file("first");
+	sync_fs();
+	// syncing after allocating a file, and running the program multiple times without
+	// compiling, gives a good demonstration of how the sync_fs funct works
 	print_fs();
 	printf("done\n");
 }
